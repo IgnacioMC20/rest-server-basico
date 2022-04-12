45f0a1deaf124f9ada993ea75f5a7a01eca4bf68
Ignacio Martínez <jm10cuyun@gmail.com>
2022-04-11 18:06:16 -0600

    lol

--- models/server.js
+++ models/server.js
@@ -37,8 +37,8 @@ class Server {
     }
 
     routes() {
+        this.app.use(this.usuariosPath, require('../routes/Usuario'));
         this.app.use(this.authPath, require('../routes/auth'));
-        this.app.use(this.usuariosPath, require('../routes/usuario'));
     }
         
     listen(){
